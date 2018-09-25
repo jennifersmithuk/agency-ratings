1b060ddae63e8e57ccc099f16a682829b007e82b
jennifersmithuk <jennifersmithuk@gmail.com>
2018-09-25 22:06:41 +0100

    feat: Basic layout

--- src/App.js
+++ src/App.js
@@ -8,16 +8,39 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App flex-container">
         <header className="App-header">
-
           <h1 className="App-title">Welcome to Agency Ratings</h1>
-          <h2 className="App-subtitle">Where Interpreters rate agencies</h2>
+          <h2 className="App-subtitle">where interpreters rate agencies they like, and the ones they don't</h2>
         </header>
       <main>
-        <p className="App-intro">
-
-        </p>
+        <div className="App-intro">
+          <p>Most interpreters are freelance, working for a variety of agencies and direct clients.</p>
+          <p>There are a multitude of agencies. Relying on word of mouth isn't enough.</p>
+          <p>Transparency, reputation, clarity: help us to help you. Rate them now.</p>
+        </div>
+        <section className="Ratings">
+        <div className="Ratings-results">
+          <h3>The best and the worst...</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+        <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+        <p>nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+        <p>reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+        <p>nulla pariatur. Excepteur sint occaecat cupidatat non proident,</p>
+        <p>sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div className="Ratings-form">
+        <h3>Rate the agency</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+        <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+        <p>nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</p>
+        <p>reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
+        <p>nulla pariatur. Excepteur sint occaecat cupidatat non proident,</p>
+        <p>sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      </section>
       </main>
 
       <footer id="footer">
